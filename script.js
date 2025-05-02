bb82b771837ca6c999739822591e2208ff72ebef
gippo05 <gsarinas17@gmail.com>
2025-05-02 08:26:28 +0800

    new weather app
    started a new project for a weather app using HTML CSS JAVASCRIPT

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const temp = document.getElementById('temp');
-
+const weather = document.getElementById('weather');
+const city = document.getElementById('city');
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=3af68efb4f427c9b53ec2c6d8bffbf4e')
 .then(res => res.json())
 .then(data => {
+    city.textContent = data.name
     temp.textContent = `Temperature: ${data.main.temp}°C`
+    weather.textContent = `Weather: ${data.weather[0].description}`
 })
 .catch(error =>  {
     console.error("Not found!!", error)
