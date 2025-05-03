0028ae9e69014457d98368f3267142533274ddd4
Gian Sarinas <gsarinas17@gmail.com>
2025-05-03 14:28:19 +0800

    made changes to design

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const weather = document.getElementById('weather');
 const city = document.getElementById('city');
 const inputEl = document.getElementById('input-el');
 const submitBtn = document.getElementById('submit-btn');
-let cityName = inputEl.value;
+
 
 
 
 
 submitBtn.addEventListener('click', () =>{
-    let cityName = inputEl.value;
+    let cityName = inputEl.value.trim();
     let searchName = cityName.toString();
+
+    if (!cityName){
+        alert("Please enter a city name!")
+        return;
+    }
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchName}&appid=3af68efb4f427c9b53ec2c6d8bffbf4e&units=metric`)
 .then(res => res.json())
 .then(data => {
