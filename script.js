da70b91a206ebc387027292b77b109374bc4122b
gippo05 <gsarinas17@gmail.com>
2025-05-03 20:58:58 +0800

    Modified javascript file. Improved error handling and clearing of error messages.

--- script.js
+++ script.js
@@ -11,14 +11,16 @@ const errorEl = document.getElementById('error-el');
 submitBtn.addEventListener('click', () =>{
     let cityName = inputEl.value.trim();
     inputEl.value = "";
-    
+    errorEl.textContent = "";
+
     if (!cityName){
         alert("Please enter a city name!")
         return;
     }
+    
 
 
-   
+
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3af68efb4f427c9b53ec2c6d8bffbf4e&units=metric`)
 .then(res => res.json())
 .then(data => {
@@ -27,9 +29,8 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3af68
     weather.textContent = `Weather: ${data.weather[0].description}`
 })
 .catch(error =>  {
-    errorEl.textContent = "City not found! Please try again."
-    errorEl.style.color = "red"
-    errorEl.style.fontSize = "20px"
-    errorEl.style.fontWeight = "bold" 
+    weather.textContent = "Weather: "
+    temp.textContent = "Temperature: "
+    errorEl.textContent = "Location not found! Please try again."
 })
 });
