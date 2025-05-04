27220f57d91a753152d658edd5b1888d8a00a87b
Gian Sarinas <gsarinas17@gmail.com>
2025-05-04 18:39:00 +0800

    Modified Javascript code for cleaner code and easier debugging using async and await

--- script.js
+++ script.js
@@ -8,29 +8,32 @@ const errorEl = document.getElementById('error-el');
 
 
 
-submitBtn.addEventListener('click', () =>{
+submitBtn.addEventListener('click', async () => {
     let cityName = inputEl.value.trim();
     inputEl.value = "";
     errorEl.textContent = "";
 
-    if (!cityName){
-        alert("Please enter a city name!")
+    if (!cityName) {
+        alert("Please enter a city name!");
         return;
     }
-    
 
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3af68efb4f427c9b53ec2c6d8bffbf4e&units=metric`);
+        
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
 
+        const data = await response.json();
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3af68efb4f427c9b53ec2c6d8bffbf4e&units=metric`)
-.then(res => res.json())
-.then(data => {
-    city.textContent = data.name
-    temp.textContent = `Temperature: ${data.main.temp}°C`
-    weather.textContent = `Weather: ${data.weather[0].description}`
-})
-.catch(error =>  {
-    weather.textContent = "Weather: "
-    temp.textContent = "Temperature: "
-    errorEl.textContent = "Location not found! Please try again."
-})
+        city.textContent = data.name;
+        temp.textContent = `${data.main.temp}°C`;
+        weather.textContent = `${data.weather[0].description}`;
+    } catch (error) {
+        city.textContent = "";
+        temp.textContent = "Temperature: ";
+        weather.textContent = "Weather: ";
+        errorEl.textContent = "Location not found! Please try again.";
+    }
 });
